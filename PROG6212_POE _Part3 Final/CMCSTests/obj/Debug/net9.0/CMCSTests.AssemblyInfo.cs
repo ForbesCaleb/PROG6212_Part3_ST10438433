2d44ea732ad86759797b91a8725f88f86755feaf
ForbesCaleb <St10438433@imconnect.edu.za>
2025-11-21 19:13:10 +0200

    Made changes to the Hr controller

--- PROG6212_POE _Part3 Final/CMCSTests/obj/Debug/net9.0/CMCSTests.AssemblyInfo.cs
+++ PROG6212_POE _Part3 Final/CMCSTests/obj/Debug/net9.0/CMCSTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCSTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3650d1713e0582eb5b96078192e6d61063d88713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3211e12aa8e7f0fcceeeb6226e7de9b883b815f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCSTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCSTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
